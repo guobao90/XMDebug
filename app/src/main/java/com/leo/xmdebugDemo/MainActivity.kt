cec806a8dcbe8082d0286f6c257f61157612ed9b
张国宝 <zhangguobao@chelun.com>
2018-12-26 20:53:15 +0800

    optimize the code

--- app/src/main/java/com/leo/xmdebugDemo/MainActivity.kt
+++ app/src/main/java/com/leo/xmdebugDemo/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.leo.xmdebug
+package com.leo.xmdebugDemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.leo.xmdebug.R
 
 class MainActivity : AppCompatActivity() {
 
