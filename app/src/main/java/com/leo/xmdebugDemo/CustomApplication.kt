b7bf6e84abde1ea6f1983a572daa6ff151fded87
guobao90 <guobao9006@gmail.com>
2019-01-05 21:24:44 +0800

    add exception collect

--- app/src/main/java/com/leo/xmdebugDemo/CustomApplication.kt
+++ app/src/main/java/com/leo/xmdebugDemo/CustomApplication.kt
@@ -6,6 +6,7 @@ import com.leo.xmdebug.utils.DebugCrashHandler
 class CustomApplication : Application() {
 
     override fun onCreate() {
+
         super.onCreate()
         DebugCrashHandler.setCrashHandler(this)
     }
